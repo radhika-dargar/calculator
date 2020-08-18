e9ed1bd3b3c04950f4b88ad7c88b54d14b81460b
radhika-dargar <radhika.dargar31@gmail.com>
2020-08-18 22:26:55 +0530

    final result

--- calculator.js
+++ calculator.js
@@ -4,49 +4,36 @@
 console.log('connected')
 
 var buttons = document.getElementsByTagName('button');
+var buttonsArr = [...buttons];
+const expArea = document.getElementById('expression');
+const solArea = document.getElementById('solution');
+solArea.innerText = '0';
+var ops = [];
 
-var arr = [...buttons]
-
-var ops = [1, 2, 3, '+', 4, 5, 6]
-var result;
-var num1 = '';
-var num2 = '';
-
-console.log(arr)
-arr.forEach(element => {
-    element.addEventListener('click', (event) => {
-        // console.log('inside')
-        // console.log(event.target.value)
-        // if (event.target.value != '=' 
-        // || event.target.value != 'clear' 
-        // || event.target.value != '+' 
-        // || event.target.value != '*'
-        // || event.target.value != '-'
-        // || event.target.value != '/'){
-        //     num1 = num1 + event.target.value
-        // } else {
-
-        // }
-
-
-        // if (event.target.value == '+') {
-        //     result = add(a, b);//
-        // }
-
-        switch (event.target.value) {
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-        }
-
-    })
+buttonsArr.forEach(element => {
+    if (element.value != 'clr' && element.value != 'eql') {
+        element.addEventListener('click', event => {
+            let value = event.target.value;
+            expArea.innerText += value;
+            ops.push(value);
+            // solve();
+        })
+    }
 });
 
-function add(a, b) {
-    return a + b;
+function solve() {
+    console.log('solving...')
+    let exp = ops.join('');
+    let result = eval(exp);
+    console.log('result', result)
+    solArea.innerText = result;
+}
+
+function clearData() {
+    console.log('clearing...')
+    ops = [];
+    expArea.innerText = '';
+    solArea.innerText = '0';
 }
 
 
